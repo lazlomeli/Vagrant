a2185893913ea88124ccd65035cc78e38976065b
Lazlo Meli <lazlomeli@gmail.com>
2022-10-05 15:43:31 +0200

    Vagrant box working

---
+++ vagrant_box/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo
+echo -e "\e[34m|=====================================================================================|\e[0m";
+echo 
+echo -e "\e[36m[#] Vagrant Setup ===>\e[0m Creating \e[21mdata\e[0m folder";
+echo -e "\e[36m[#] Vagrant Setup ===>\e[0m You can now run \e[21mvagrant up\e[0m command to run the project";
+echo 
+echo -e "\e[34m|===================================[by Lazlo Meli]===================================|\e[0m";
