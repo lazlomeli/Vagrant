7e0546d088603ad9172a617ffee8da89c2daf5af
Lazlo Meli <lazlomeli@gmail.com>
2022-10-05 15:53:02 +0200

    hotfix: Updated Vagrant file

--- vagrant_box/setup.sh
+++ vagrant_box/setup.sh
@@ -3,6 +3,7 @@ echo
 echo -e "\e[34m|=====================================================================================|\e[0m";
 echo 
 echo -e "\e[36m[#] Vagrant Setup ===>\e[0m Creating \e[21mdata\e[0m folder";
+echo -e "\e[36m[#} Vagrant Setup ===>\e[0m Open a tab at 127.0.0.1:8000 once vagrant up command finishes";
 echo -e "\e[36m[#] Vagrant Setup ===>\e[0m You can now run \e[21mvagrant up\e[0m command to run the project";
 echo 
 echo -e "\e[34m|===================================[by Lazlo Meli]===================================|\e[0m";
