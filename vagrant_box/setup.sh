77cf5fafd9f7f97c534799bd9e30f309c82cf1db
Lazlo Meli <lazlomeli@gmail.com>
2022-10-06 12:01:24 +0200

    feat: Updated setup.sh

--- vagrant_box/setup.sh
+++ vagrant_box/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+LB="\e[36m"; # LIGHT BLUE
+B="\e[34m"; # BLUE
+GR="\e[92m"; # GREEN
+UDL="\e[21m"; # UNDERLINED
+NM="\e[0m"; # NORMAL
+
 echo
-echo -e "\e[34m|=====================================================================================|\e[0m";
+echo -e "${B}|=====================================================================================|${NM}";
 echo 
-echo -e "\e[36m[#] Vagrant Setup ===>\e[0m Creating \e[21mdata\e[0m folder";
-echo -e "\e[36m[#} Vagrant Setup ===>\e[0m Open a tab at 127.0.0.1:8000 once vagrant up command finishes";
-echo -e "\e[36m[#] Vagrant Setup ===>\e[0m You can now run \e[21mvagrant up\e[0m command to run the project";
+echo -e "${LB}[#] Vagrant Setup ===>${NM} Creating ${UDL}data${NM} folder";
+sleep 1s; echo -e "${LB}[#] Vagrant Setup ===>${NM} ${GR}DONE${NM}";
+echo -e "${LB}[#] Vagrant Setup ===>${NM} Open a tab at 127.0.0.1:8000 once vagrant up command finishes";
+sleep 1s;
+echo -e "${LB}[#] Vagrant Setup ===>${NM} You can now run ${UDL}vagrant up${NM} command to run the project";
 echo 
-echo -e "\e[34m|===================================[by Lazlo Meli]===================================|\e[0m";
+echo -e "${B}|===================================[by Lazlo Meli]===================================|${NM}";
