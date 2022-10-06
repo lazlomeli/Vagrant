e96f5a02db1b7aa10fb9fad0a77cb2d6a15f0570
Lazlo Meli <lazlomeli@gmail.com>
2022-10-06 20:28:47 +0200

    update: Updated setup file v2

--- vagrant_box/setup.bat
+++ vagrant_box/setup.bat
@@ -1,4 +1,4 @@
 msg "%username%" Configuring and creating your Vagrant box. A browser tab will open at (127.0.0.1:8080) when the proccess ends.
 mkdir data
 vagrant up
-start chrome https://127.0.0.1:8080/
+start chrome http://127.0.0.1:8080/
